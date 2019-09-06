2ea06d6fa1ee992db4da894f8e4549fa9b371ab5
18222478537@163.com <18222478537@163.com>
2019-09-06 14:22:37 +0800

    logo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,66 @@
 import React from 'react'
+import { Menu} from 'antd';
+import './index.less'
+const { SubMenu } = Menu;
 
-function Navbar (){
-   return (
-       <div>this is Navbar</div>
-   )
+function Navbar() {
+    return (
+        <div>
+            <div className="logo">
+                <img src="/logo-ant.svg" alt=""/>
+                <h1>Imooc MS</h1>
+            </div>
+            <Menu theme="dark" style={{ width: 256 }} mode="vertical">
+                <SubMenu
+                    key="sub1"
+                    title={
+                        <span>
+                            
+                            <span>Navigation One</span>
+                        </span>
+                    }
+                >
+                    <Menu.ItemGroup title="Item 1">
+                        <Menu.Item key="1">Option 1</Menu.Item>
+                        <Menu.Item key="2">Option 2</Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup title="Iteom 2">
+                        <Menu.Item key="3">Option 3</Menu.Item>
+                        <Menu.Item key="4">Option 4</Menu.Item>
+                    </Menu.ItemGroup>
+                </SubMenu>
+                <SubMenu
+                    key="sub2"
+                    title={
+                        <span>
+                           
+                            <span>Navigation Two</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key="5">Option 5</Menu.Item>
+                    <Menu.Item key="6">Option 6</Menu.Item>
+                    <SubMenu key="sub3" title="Submenu">
+                        <Menu.Item key="7">Option 7</Menu.Item>
+                        <Menu.Item key="8">Option 8</Menu.Item>
+                    </SubMenu>
+                </SubMenu>
+                <SubMenu
+                    key="sub4"
+                    title={
+                        <span>
+                            
+                            <span>Navigation Three</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key="9">Option 9</Menu.Item>
+                    <Menu.Item key="10">Option 10</Menu.Item>
+                    <Menu.Item key="11">Option 11</Menu.Item>
+                    <Menu.Item key="12">Option 12</Menu.Item>
+                </SubMenu>
+            </Menu>
+        </div>
+    )
 }
 export default Navbar
